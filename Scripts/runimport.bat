6c1381aeeede0ddbeeaf2d2a00002d9baa3c7b34
mikecoker <mikecoker@playfulcorp.com>
2019-06-26 09:28:08 -0500

    added an example of how to reimport a datatable

--- Scripts/runimport.bat
+++ Scripts/runimport.bat
@@ -1,1 +1,1 @@
-UE4Editor-Cmd.exe "C:\Users\calem\Documents\Unreal Projects\UnrealImportAssetsExamples\AssetImporting.uproject" -run=ImportAssets -importsettings="C:\Users\calem\Documents\Unreal Projects\UnrealImportAssetsExamples\Scripts\ExampleAssetsImportSettings.json" -nosourcecontrol
+UE4Editor-Cmd.exe "C:\projects\UnrealImportAssetsExamples\AssetImporting.uproject" -run=ImportAssets -importsettings="C:\Projects\UnrealImportAssetsExamples\Scripts\ExampleAssetsImportSettings.json" -nosourcecontrol -noshadercompile
--- Source/AssetImporting/AssetImportingGameModeBase.h
+++ Source/AssetImporting/AssetImportingGameModeBase.h
@@ -3,8 +3,24 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/DataTable.h"
 #include "GameFramework/GameModeBase.h"
 #include "AssetImportingGameModeBase.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FExampleData : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	FExampleData() {}
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FText Name;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) float Health = 0.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) float Mana = 0.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) float Attack = 2.f;
+};
 
 /**
  * 
